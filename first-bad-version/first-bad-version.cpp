bf306eb08bbd3892e350d4bd301afdf7f2c08a09
Soumyashree Goswami <49723755+soumee1902@users.noreply.github.com>
2021-12-20 12:02:04 +0530

    Time: 0 ms (100.00%), Space: 6 MB (21.17%) - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -9,22 +9,22 @@ public:
         int low = 1;
         int mid = low+(high-low)/2;
         
-        while(low <= high)
+        while(low<=high)
         {
             mid = low+(high-low)/2;
             
             if(isBadVersion(mid) == true)
             {
-                high = mid - 1;
-                
+                high = mid-1;
             }
             
-            else{
-                low = mid + 1;
+            if(isBadVersion(mid) == false)
+            {
+                low = mid+1;
             }
+            
         }
         
         return low;
-    
     }
 };
